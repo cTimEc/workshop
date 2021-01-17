ee3c330be2915fc7c3b384e6f7d48824413f4c9c
cTimEc <kongyod.j@ku.th>
2021-01-17 10:06:21 +0700

    add ex

---
+++ โฟลเดอร์ใหม่/ex.py
@@ -0,0 +1,32 @@
+# จงแสดง "banana"
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+# จงแก้ไขข้อมูลจาก "apple" เป็น "kiwi"
+thislist = ["apple", "banana", "cherry"]
+thislist[0] = "kiwi"
+print(thislist)
+
+# จงเพิ่ม "kiwi" ไปยัง fruits list
+fruits = ["apple", "banana", "cherry"]
+fruits.add("kiwi")
+print(fruits)
+
+# จงเพิ่ม "lemon" ไประหว่าง "apple" กับ "ิิbananna"
+fruits = ["apple", "banana", "cherry"]
+fruits.add("lemon"[1])
+print(fruits)
+
+# จงลบ "cherry" จาก list
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("cherry")
+print(fruits)
+
+
+# จงแสดงตัวสุดท้ายของ fruits
+fruits = ["apple", "banana", "cherry"]
+print(fruits[2])
+
+# จงแสดงจำนวนของ fruits
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
