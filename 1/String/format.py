55533dccca74d29f1038219ae6882200fc3a89e4
cTimEc <kongyod.j@ku.th>
2021-03-16 15:16:31 +0700

    add files string

---
+++ 1/String/format.py
@@ -0,0 +1,6 @@
+age = 20
+txt = "My name is TIME, and I am {}"
+
+result = txt.format(age)
+
+print("result", result)
---
+++ 1/String/stirng.py
@@ -0,0 +1,6 @@
+name = "TIME"
+age = 21
+
+result = f" My name is {name} , and Iam {age}"
+
+print("result", result)
