ea48e9399de332b8552e2469eadf7b77a5acc59b
cTimEc <kongyod.j@ku.th>
2021-03-16 13:50:06 +0700

    add Operators

---
+++ 1/operators/Comparison operators.py
@@ -0,0 +1,9 @@
+x = 10
+y = 12
+
+print("x > y is", x > y)  # x > y: False
+print("x < y is", x < y)  # x < y: True
+print("x == y is", x == y)  # x == y: False
+print("x != y is", x != y)  # x != y: True
+print("X >= y is", x >= y)  # x >= y: False
+print("X <= y is", x <= y)  # x <= y: True
